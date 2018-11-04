23d2ff4d255dac4a04d3b909e0d646100b6af413
warade <warade.anshul@gmail.com>
2018-11-04 15:30:53 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err) console.log('Unable to connect to server...');
+	});
+	next();
+});
+
+// app.use((req, res, nextt) => {
+// 	return res.render('maintainance.hbs', {
+// 		errorMessage: 'Site is under maintainance...'
+// 	});
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home page',
+		welcomeMessage: 'Welcome to my website.'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About page',
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: '404 Not Found!'
+	})
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000!');
+});
