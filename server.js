cc74f0100d53ce77ef372a85eed9ac814c4c750f
warade <warade.anshul@gmail.com>
2018-11-04 18:39:39 +0530

    Adding projects.hbs

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/bad', (req, res) => {
 	})
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects page'
+	});
+});	
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}!`);
 });
